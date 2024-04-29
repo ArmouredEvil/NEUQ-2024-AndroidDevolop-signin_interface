c661944564ffe8abd8356087d670acdac3c4f38c
ArmouredEvil <1928774641@qq.com>
2024-04-29 15:40:42 +0800

    test

---
+++ app/src/main/java/com/example/android_signin_interface/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.android_signin_interface;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 关联activity.xml
+        setContentView(R.layout.activity_main);
+        
+    }
+}
